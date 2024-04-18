77978689d06abbb29d0a8c4b7574f48dc2c86901
vikash-kanauja <vikashkanoujia@gmail.com>
2024-04-18 17:11:54 +0530

    optimized code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const popup = document.getElementById('popup');
 const popupImage = document.getElementById('popup-image');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
-
+const image = document.getElementsByClassName("galleryImg");
 const images = [
     "./images/1.jpg",
     "./images/2.jpg",
@@ -32,17 +32,26 @@ let currentImageIndex = 0;
 images.forEach((imageUrl, index) => {
     const img = document.createElement('img');
     img.classList.add(
+        "galleryImg",
         "block",
         "w-[250px]",
         "h-[250px]",
         "2xl:w-[300px]",
         "2xl:h-[300px]"
-    )
+    );
+    img.dataset.index = index;
     img.src = imageUrl;
     img.alt = `Image ${index + 1}`;
-    img.addEventListener('click', () => openPopup(index));
+    // img.addEventListener('click', () => openPopup(index));
     gallery.appendChild(img);
 });
+
+// Assuming gallery is the parent element containing all the img elements
+gallery.addEventListener('click', (event) => {
+        const dataIndex = event.target.dataset.index;
+        openPopup(dataIndex);
+});
+
 
 // Open popup
 function openPopup(index) {
@@ -84,13 +93,15 @@ prevBtn.addEventListener('click', () => {
 // show and hidde next and previous button
 const showAndHideNavButtons = () => {
     if (currentImageIndex == 0) {
-        prevBtn.classList.add("invisible")
+        prevBtn.style.visibility = "hidden";
+        nextBtn.style.visibility = "visible";
     }
     else if (currentImageIndex == images.length - 1) {
-        nextBtn.classList.add("invisible")
+        prevBtn.style.visibility = "visible";
+            nextBtn.style.visibility = "hidden";
     }
     else {
-        prevBtn.classList.remove("invisible")
-        nextBtn.classList.remove("invisible")
+        prevBtn.style.visibility = "visible";
+            nextBtn.style.visibility = "visible";
     }
 }
